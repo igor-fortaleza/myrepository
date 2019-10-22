4acb4f7393aaadea577311b97ff64da1a0fa59a6
Igor Alves Fortaleza <mt13077@interfile.com.br>
2019-10-22 08:15:12 -0300

    21/10

--- Codes/AulaOsmar/Asp.Net/WebLoja/WebLoja/BD/Db_produto.cs
+++ Codes/AulaOsmar/Asp.Net/WebLoja/WebLoja/BD/Db_produto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -14,13 +15,54 @@ namespace WebLoja.BD
 
         public Db_Produto()
         {
-            _conn = ConfigurationManager.ConnectionStrings["WebLojaConnectionString"].ToString();
+            _conn = ConfigurationManager.ConnectionStrings["WebLojaConnection"].ToString();
+        }
+
+        public bool CreatProduto(Produto produto)
+        {
+            bool criado = false;
+            SqlConnection connection = new SqlConnection(_conn);
+            SqlCommand command = new SqlCommand("INSERT Produtos(Descricao) VALUES (@Descricao)", connection);
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@Descricao", produto.Descricao);
+
+            int linhaAfetadas = 0;
+            connection.Open();
+            linhaAfetadas = command.ExecuteNonQuery();
+            connection.Close();
+
+            if (linhaAfetadas > 0)
+                criado = true;
+
+            return criado;
+        }
+
+        public Produto GetOneProduto(int id)
+        {
+            SqlConnection connection = new SqlConnection(_conn);
+            SqlCommand command = new SqlCommand("Select ID, Descricao FROM Produtos WHERE ID = @ID", connection);
+            command.CommandType = CommandType.Text;
+            command.Parameters.AddWithValue("@ID", id);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            Produto produto = new Produto();
+            if(reader.Read())
+            {
+                produto.ProdutoId = Convert.ToInt32(reader["ID"]);
+                produto.Descricao = Convert.ToString(reader["Descricao"]);
+            }
+            connection.Close();
+
+            return produto;
+
         }
 
         public List<Produto> ListProdutos()
         {
             SqlConnection connection = new SqlConnection(_conn);
-            SqlCommand command = new SqlCommand("", connection);
+            SqlCommand command = new SqlCommand("Select ID, Descricao FROM Produtos", connection);
 
             connection.Open();
             SqlDataReader reader = command.ExecuteReader();
@@ -29,8 +71,8 @@ namespace WebLoja.BD
             while(reader.Read())
             {
                 Produto produto = new Produto();
-                produto.ProdutoId = Convert.ToInt32(reader["ProdutoId"]);
-                produto.ProdutoId = Convert.ToInt32(reader["Descricao"]);
+                produto.ProdutoId = Convert.ToInt32(reader["ID"]);
+                produto.Descricao = Convert.ToString(reader["Descricao"]);
 
                 listProdutos.Add(produto);
             }
--- Codes/AulaOsmar/Asp.Net/WebLoja/WebLoja/Controllers/ProdutoController.cs
+++ Codes/AulaOsmar/Asp.Net/WebLoja/WebLoja/Controllers/ProdutoController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebLoja.BD;
+using WebLoja.Models;
 
 namespace WebLoja.Controllers
 {
     public class ProdutoController : Controller
     {
+        private Db_Produto _db = new Db_Produto();
         // GET: Produto
         public ActionResult Index()
         {
+            List<Produto> listprodutos = _db.ListProdutos();
+            return View(listprodutos);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Produto produto = _db.GetOneProduto(id);
+
+            return View(produto);
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Produto produto)
+        {
+            bool criado = _db.CreatProduto(produto);
+
+            return RedirectToAction("Index");
         }
     }
 }
